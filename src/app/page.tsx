36bb0cf17cfc908a811425f9d1019fac3480594a
kanamaru2525 <mfkanamaru@gmail.com>
2024-06-19 18:38:53 +0900

    [add]portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,80 @@
-import Image from "next/image";
+import Header from "@/component/header";
+import { Box, Center, Flex, Grid, GridItem, List, ListItem, Motion ,Text,Image} from "@yamada-ui/react";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box _scrollbar={{display:"none"}} fontFamily="notojp">
+      <Header />
+      <Flex h="100vh" bg="#B9C0CC" justifyContent="center" alignItems="center" flexDirection="column" display="flex">   
+          <Motion fontSize="96px"fontWeight="black"color="white" borderBottom="solid">
+            KANAMARU TAISEI
+          </Motion>
+          <Text fontSize="32px" color="white" margin="25px">A failure to become a creator</Text>
+      </Flex>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Box h="10vh"justifyContent="center" alignItems="center"display="flex">
+        <Text marginRight="25px"marginLeft="25px"paddingTop="5"fontSize="32px"fontWeight="black">Profile</Text>
+      </Box>
+        <Box h="90vh"justifyContent="center" alignItems="center" display="flex">
+          <Flex flexWrap="wrap">
+            <Image src="/images/me.jpg" alt="me" zoom="0.25"marginRight="25px"marginLeft="25px" padding="5" />
+            <List fontSize="40px"marginRight="25px"marginLeft="25px"paddingTop="5" >
+              <ListItem>Name:金丸汰生</ListItem>
+              <ListItem>Age:20</ListItem>
+              <ListItem>Hobby:TRPG・アナログゲーム</ListItem>
+              <ListItem>From:Awaji</ListItem>
+            </List>
+          </Flex>
+        </Box>
+          <Box h="full" bg="#2F4A78">
+            <Box h="10vh"justifyContent="center" alignItems="center"display="flex">
+            <Text marginRight="25px"marginLeft="25px"paddingTop="5"fontSize="32px"fontWeight="black"color={"white"}>Skill</Text>
+            </Box>
+          <Box>
+          <Center>
+            <Grid templateColumns="repeat(2, 1fr)" gap={"md"} margin="5vh" w="65vw">
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/figma.png" alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%" textAlign="center">Figma</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★★★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/htmls.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%" textAlign="center">HTMLL/Javascript/CSS</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/Nextjs.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%"textAlign="center">Next.js</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/daVinci.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%"textAlign="center">DaVinci Resolve</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:★★</Text>
+              </GridItem>
+              <GridItem w="full"  rounded="md" bg="#ffffff">
+                  <Center>
+                    <Image src="/images/colors.png"  alt="me" zoom="0.5"/>
+                  </Center>
+                  <Text fontSize="40px" w="100%"textAlign="center">色彩検定</Text>
+                  <Text fontSize="40px" w="100%" textAlign="center">習熟度:2級</Text>
+              </GridItem>
+            </Grid>
+          </Center>
+        </Box>  
+        </Box>
+        
+        <Footer />  
+    </Box>
   );
 }
