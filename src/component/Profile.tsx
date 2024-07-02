ecff276eabe4b7c85da4c4fd3776cf902aff5f80
kanamaru2525 <mfkanamaru@gmail.com>
2024-07-02 09:17:16 +0900

    profile　フォントサイズ変更

--- src/component/Profile.tsx
+++ src/component/Profile.tsx
@@ -3,7 +3,7 @@ import { Box, Image, Flex, List, ListItem, Center } from "@yamada-ui/react"
 const Profile = () => {
     return (
         <div>        
-        <Box h="90vh"justifyContent="center" alignItems="center" display={{ base: "flex", lg: "none", }}>
+        <Box h="50vh"justifyContent="center" alignItems="center" display={{ base: "flex", lg: "none", }}>
           <Flex flexWrap="wrap">
             <Image src="\images\me.jpg" alt="me" zoom="0.25"marginRight="25px"marginLeft="25px" padding="5" />
             <List fontSize="6xl"marginRight="25px"marginLeft="25px"paddingTop="5" >
@@ -19,7 +19,7 @@ const Profile = () => {
           
         <Flex flexDirection="column">
             <Image src="\images\me.jpg" alt="me" zoom="0.25" padding="5" />
-            <List fontSize="2lx"marginRight="25px"marginLeft="25px"paddingTop="5" >
+            <List fontSize="2lx"margin="25px">
               <ListItem>Name:金丸汰生</ListItem>
               <ListItem>Age:20</ListItem>
               <ListItem>Hobby:TRPG・アナログゲーム</ListItem>
